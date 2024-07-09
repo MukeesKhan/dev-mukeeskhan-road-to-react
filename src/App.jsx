e68ebc876bf9cb9090a40ad670d90545d99b62db
mukees <mukees.khan@cloudmizan.com>
2024-07-09 22:56:34 +0500

    React Components Instantiation

--- src/App.jsx
+++ src/App.jsx
@@ -26,27 +26,44 @@ function App()
     <div>
       <h1>Road To React</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search />
 
       <hr /> {/*Horizontal Break syntax in html */}
 
-      <ul> {/*Unordered list tag in html*/}
-        {
-          list.filter((obj) => obj.num_comments>0).map(function (item){
-            return (<li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span> {item.author} </span>
-              <span> {item.num_comments} </span>
-              <span> {item.points} </span>
-            </li>);
-          })
-        }
-      </ul>
+      <List /> {/*Calling another component*/}
+
     </div>
   );
+}
+
+function List()
+{
+  return (
+  <ul> {/*Unordered list tag in html*/}
+  {
+    list.filter((obj) => obj.num_comments>0).map(function (item){
+      return (<li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span> {item.author} </span>
+        <span> {item.num_comments} </span>
+        <span> {item.points} </span>
+      </li>);
+    })
+  }
+</ul>
+  )
+}
+
+function Search()
+{
+  return(
+    <div>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" />
+    </div>
+  )
 }
 
 export default App
